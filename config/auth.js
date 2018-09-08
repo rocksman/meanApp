e32285f2f6992ceb13521c15003c340f7175639e
rocksman <ronsam98@gmail.com>
2018-09-08 18:53:13 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1024948137707219', // your App ID
         'clientSecret'  : '03601ebb0a604f80d9af78ae7d255356', // your App Secret
-        'callbackURL'   : 'http://https://blooming-river-36318.herokuapp.com/auth/facebook/callback',
+        'callbackURL'   : 'https://blooming-river-36318.herokuapp.com/auth/facebook/callback',
         'profileURL'    : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email',
         'profileFields' : ['id', 'email', 'name'] // For requesting permissions from Facebook API
     }
